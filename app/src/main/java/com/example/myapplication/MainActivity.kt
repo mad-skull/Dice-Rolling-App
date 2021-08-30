bc5c7b5d6559cec2cb044517141406627b22d990
mad-skull <da.madskull@gmail.com>
2021-08-30 22:52:24 +0530

    completed functions

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
 
         val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
-        
+
 
     }
 }
