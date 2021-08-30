8e8f31cb1bbf9efcda0dab884df601b75b43e20b
mad-skull <da.madskull@gmail.com>
2021-08-30 22:33:07 +0530

    Functional dice roller

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text= "Let's Roll"
+        //rollButton.text= "Let's Roll"
         rollButton.setOnClickListener {
             //Toast.makeText(this, text: "Button Clicked", Toast.LENGYH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+        //resultText.text = "Rolling"
+
     }
 }
